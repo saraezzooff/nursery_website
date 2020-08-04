6366b1f2b420194f33234d2ffabe6e3e11096480
saraezzooff <58671265+saraezzooff@users.noreply.github.com>
2020-08-04 21:27:59 +0200

    Update shrink-navbar.js

--- js/shrink-navbar.js
+++ js/shrink-navbar.js
@@ -1,3 +1,4 @@
+/*global console */
 var arrOfInputs = [];
 const navbar = document.querySelector('#main_navbar');
 const btn1 = document.querySelector('#btn-1');
@@ -20,8 +21,9 @@ const btn5_active = document.querySelector('#btn5-active');
 const btn6_active = document.querySelector('#btn6-active');
 
 var pathname = window.location.pathname;
-console.log(pathname);
 
+
+console.log($(document).width());
 function iteratorAddFunction(input) {
     navbar.classList.add('navbar-shrink');
     for (var i = 0; i < arrOfInputs.length; i++) {
@@ -50,29 +52,29 @@ function iteratorRemoveFunction(input) {
 const dropdown = document.querySelector('.dropdown-menu');
 window.addEventListener('scroll', function () {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        if (pathname === '/project/index.html') { 
+        if (pathname.includes('/index.html')) { 
             
             btn1_active.classList.add('btn1-shrink');
             iteratorAddFunction(btn1); 
         }
-        else if (pathname === '/project/about.html') {
+        else if (pathname.includes('/about.html')) {
 
             btn2_active.classList.add('btn1-shrink');
             iteratorAddFunction(btn2); 
         }
-        else if (pathname === '/project/events.html') {
+        else if (pathname.includes('/events.html')) {
             btn3_active.classList.add('btn1-shrink');
             iteratorAddFunction(btn3); 
         }
-        else if (pathname === '/project/gallery.html') {
+        else if (pathname.includes('/gallery.html')) {
             btn4_active.classList.add('btn1-shrink');
             iteratorAddFunction(btn4); 
         }
-        else if (pathname === '/project/faq.html') {
+        else if (pathname.includes('/faq.html')) {
             btn5_active.classList.add('btn1-shrink');
             iteratorAddFunction(btn5); 
         }
-        else if (pathname === '/project/contacts.html') {
+        else if (pathname.includes('/contacts.html')){
             btn6_active.classList.add('btn1-shrink');
             iteratorAddFunction(btn6); 
         }
@@ -81,30 +83,30 @@ window.addEventListener('scroll', function () {
     //////////////////////////////////////////////////////////////
 
     else if (document.body.scrollTop < 80 || document.documentElement.scrollTop < 80) {
-        if (pathname === '/project/index.html') {
+        if (pathname.includes('/index.html'))  {
             
             btn1_active.classList.remove('btn1-shrink');
             iteratorRemoveFunction(btn1); 
 
         }
-        else if (pathname === '/project/about.html') {
+        else if (pathname.includes('/about.html')) {
             btn2_active.classList.remove('btn1-shrink');
             iteratorRemoveFunction(btn2); 
         }
-        else if (pathname === '/project/events.html') {
+        else if (pathname.includes('/events.html')) {
            
             btn3_active.classList.remove('btn1-shrink');
             iteratorRemoveFunction(btn3); 
         }
-        else if (pathname === '/project/gallery.html') {
+        else if (pathname.includes('/gallery.html')) {
             btn4_active.classList.remove('btn1-shrink');
             iteratorRemoveFunction(btn4); 
         }
-        else if (pathname === '/project/faq.html') {
+        else if (pathname.includes('/faq.html')) {
             btn5_active.classList.remove('btn1-shrink');
             iteratorRemoveFunction(btn5); 
         }
-        else if (pathname === '/project/contacts.html') {
+        else if (pathname.includes('/contacts.html')) {
             btn6_active.classList.remove('btn1-shrink');
             iteratorRemoveFunction(btn6); 
         }
@@ -114,4 +116,186 @@ window.addEventListener('scroll', function () {
 
 
 
+}); 
+
+
+/*
+
+const navbar = document.querySelector('#main_navbar');
+const btn1 = document.querySelector('#btn-1');
+const btn2 = document.querySelector('#btn-2');
+const btn3 = document.querySelector('#btn-3');
+const btn4 = document.querySelector('#btn-4');
+const btn5 = document.querySelector('#btn-5');
+const btn6 = document.querySelector('#btn-6');
+const btn1_active = document.querySelector('#btn1-active');
+const btn2_active = document.querySelector('#btn2-active');
+const btn3_active = document.querySelector('#btn3-active');
+const btn4_active = document.querySelector('#btn4-active');
+const btn5_active = document.querySelector('#btn5-active');
+const btn6_active = document.querySelector('#btn6-active');
+
+
+
+var pathname = window.location.pathname;
+console.log(pathname);
+
+const dropdown = document.querySelector('.dropdown-menu');
+window.addEventListener('scroll',function(){
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
+          {
+              if(pathname === '/index.html')
+                  {
+                      navbar.classList.add('navbar-shrink');
+                      btn1_active.classList.add('btn1-shrink');
+                      btn2.classList.add('btn-shrink');
+                      btn3.classList.add('btn-shrink');
+                      btn4.classList.add('btn-shrink');
+                      btn5.classList.add('btn-shrink');
+                      btn6.classList.add('btn-shrink');
+                      console.log(dropdown.style.background);
+                      dropdown.style.marginTop = "-35px";
+                  }
+              else if(pathname === '/about.html'){
+                      navbar.classList.add('navbar-shrink');
+                      btn1.classList.add('btn-shrink');
+                      btn2_active.classList.add('btn1-shrink');
+                      btn3.classList.add('btn-shrink');
+                      btn4.classList.add('btn-shrink');
+                      btn5.classList.add('btn-shrink');
+                      btn6.classList.add('btn-shrink');
+                      console.log(dropdown.style.background);
+                      dropdown.style.marginTop = "-35px";
+              }
+              else if (pathname === '/events.html'){
+                      navbar.classList.add('navbar-shrink');
+                      btn1.classList.add('btn-shrink');
+                      btn2.classList.add('btn-shrink');
+                      btn3_active.classList.add('btn1-shrink');
+                      btn4.classList.add('btn-shrink');
+                      btn5.classList.add('btn-shrink');
+                      btn6.classList.add('btn-shrink');
+                      console.log(dropdown.style.background);
+                      dropdown.style.marginTop = "-35px";
+              }
+              else if (pathname === '/gallery.html'){
+                      navbar.classList.add('navbar-shrink');
+                      btn1.classList.add('btn-shrink');
+                      btn2.classList.add('btn-shrink');
+                      btn3.classList.add('btn-shrink');
+                      btn4_active.classList.add('btn1-shrink');
+                      btn5.classList.add('btn-shrink');
+                      btn6.classList.add('btn-shrink');
+                      console.log(dropdown.style.background);
+                      dropdown.style.marginTop = "-35px";
+              }
+              else if (pathname === '/faq.html'){
+                      navbar.classList.add('navbar-shrink');
+                      btn1.classList.add('btn-shrink');
+                      btn2.classList.add('btn-shrink');
+                      btn3.classList.add('btn-shrink');
+                      btn4.classList.add('btn-shrink');
+                      btn5_active.classList.add('btn1-shrink');
+                      btn6.classList.add('btn-shrink');
+                      console.log(dropdown.style.background);
+                      dropdown.style.marginTop = "-35px"; 
+              }
+              else if (pathname === '/contacts.html'){
+                      navbar.classList.add('navbar-shrink');
+                      btn1.classList.add('btn-shrink');
+                      btn2.classList.add('btn-shrink');
+                      btn3.classList.add('btn-shrink');
+                      btn4.classList.add('btn-shrink');
+                      btn5.classList.add('btn-shrink');
+                      btn6_active.classList.add('btn1-shrink');
+                      console.log(dropdown.style.background);
+                      dropdown.style.marginTop = "-35px";   
+              }
+    
+          }
+    else if(document.body.scrollTop < 80 || document.documentElement.scrollTop < 80)
+        {
+            if(pathname === '/index.html')
+                
+                {
+                      navbar.classList.remove('navbar-shrink');
+                      btn1_active.classList.remove('btn1-shrink');
+                      btn2.classList.remove('btn-shrink');
+                      btn3.classList.remove('btn-shrink');
+                      btn4.classList.remove('btn-shrink');
+                      btn5.classList.remove('btn-shrink');
+                      btn6.classList.remove('btn-shrink');
+                      dropdown.style.marginTop = "-1px";
+         
+                }
+            else if(pathname === '/about.html')
+                {
+                      navbar.classList.remove('navbar-shrink');
+                      btn1.classList.remove('btn-shrink');
+                      btn2_active.classList.remove('btn1-shrink');
+                      btn3.classList.remove('btn-shrink');
+                      btn4.classList.remove('btn-shrink');
+                      btn5.classList.remove('btn-shrink');
+                      btn6.classList.remove('btn-shrink');
+                      dropdown.style.marginTop = "-1px";  
+                }
+            else if (pathname === '/events.html'){
+                      navbar.classList.remove('navbar-shrink');
+                      btn1.classList.remove('btn-shrink');
+                      btn2.classList.remove('btn-shrink');
+                      btn3_active.classList.remove('btn1-shrink');
+                      btn4.classList.remove('btn-shrink');
+                      btn5.classList.remove('btn-shrink');
+                      btn6.classList.remove('btn-shrink');
+                      dropdown.style.marginTop = "-1px";  
+            }
+            else if (pathname === '/gallery.html'){
+                      navbar.classList.remove('navbar-shrink');
+                      btn1.classList.remove('btn-shrink');
+                      btn2.classList.remove('btn-shrink');
+                      btn3.classList.remove('btn-shrink');
+                      btn4_active.classList.remove('btn1-shrink');
+                      btn5.classList.remove('btn-shrink');
+                      btn6.classList.remove('btn-shrink');
+                      dropdown.style.marginTop = "-1px";    
+            }
+            else if (pathname === '/faq.html'){
+                      navbar.classList.remove('navbar-shrink');
+                      btn1.classList.remove('btn-shrink');
+                      btn2.classList.remove('btn-shrink');
+                      btn3.classList.remove('btn-shrink');
+                      btn4.classList.remove('btn-shrink');
+                      btn5_active.classList.remove('btn1-shrink');
+                      btn6.classList.remove('btn-shrink');
+                      dropdown.style.marginTop = "-1px";   
+            }
+            else if (pathname === '/contacts.html'){
+                      navbar.classList.remove('navbar-shrink');
+                      btn1.classList.remove('btn-shrink');
+                      btn2.classList.remove('btn-shrink');
+                      btn3.classList.remove('btn-shrink');
+                      btn4.classList.remove('btn-shrink');
+                      btn5.classList.remove('btn-shrink');
+                      btn6_active.classList.remove('btn1-shrink');
+                      dropdown.style.marginTop = "-1px"; 
+            }
+            
+            
+        }
+  
+    
+    
 });
+window.onscroll = function()
+{
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
+          {
+                  navbar.classList.add('navbar-shrink');
+    
+          }
+    else if(document.body.scrollTop < 80 || document.documentElement.scrollTop < 80)
+        {
+            navbar.classList.remove('navbar-shrink');
+        }
+
+};*/
